f03fc32ef540038d335972857f3d1281b01ee718
Ben Mumford <bmumford@scottlogic.com>
2015-04-10 10:26:11 +0100

    src and pom from spring boot initializr

---
+++ src/main/java/com/benmumford/SpringAngularNotesApplication.java
@@ -0,0 +1,12 @@
+package com.benmumford;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAngularNotesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/benmumford/SpringAngularNotesApplicationTests.java
@@ -0,0 +1,18 @@
+package com.benmumford;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringAngularNotesApplication.class)
+@WebAppConfiguration
+public class SpringAngularNotesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
