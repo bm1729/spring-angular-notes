a98d036d03177f059f57cf70807bd88ec5741a0d
Ben Mumford <bmumford@scottlogic.com>
2015-04-13 13:38:03 +0100

    First stab at the notes page

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -7,8 +7,24 @@ var appModule = angular.module('app', ['ngRoute'])
 			templateUrl : 'templates/tags.html',
 			controller : 'tagsController'
 		}).otherwise('/');
-	}).controller('notesController', function($scope) {
-		$scope.message = 'notes';
+	}).controller('notesController', function($scope, $http) {
+		
+		$scope.notes = [];
+		
+		var initialise = function() {
+			$http.get('/notes').success(function(data) {
+				$scope.notes = _.map(data, function(datum) {
+					return {
+						id: datum.id,
+						title: datum.title,
+						modifiedDate: moment(datum.modifiedDate).format('MMMM Do YYYY, h:mm a')
+					};
+				});
+			});
+		};
+		
+		initialise();
+		
 	}).controller('tagsController', function($scope) {
 		$scope.message = 'tags';
 	});
